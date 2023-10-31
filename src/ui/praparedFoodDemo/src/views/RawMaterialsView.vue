038e4c12ebdd78a534469e12a1d7801801020483
Yillian4372 <wzhe0769@gmail.com>
2023-10-31 17:24:25 +0800

    原料页面v1.1

--- src/ui/praparedFoodDemo/src/views/RawMaterialsView.vue
+++ src/ui/praparedFoodDemo/src/views/RawMaterialsView.vue
@@ -4,8 +4,6 @@
 </template>
 
 <script setup>
-// import { defineConfig } from 'vite';
-// import vue from '@vitejs/plugin-vue';
 import { onMounted, ref, inject } from 'vue';
 import RawMaterialsListView from '../components/RawMaterialsListView.Vue';
 
