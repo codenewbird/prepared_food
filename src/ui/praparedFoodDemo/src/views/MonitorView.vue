ad20440f18e7b6cf7f9092ed44e2f755fca7cf63
cloud-lost-night <tyzsmx@qq.com>
2023-10-31 17:09:14 +0800

    监测UI初步设计

--- src/ui/praparedFoodDemo/src/views/MonitorView.vue
+++ src/ui/praparedFoodDemo/src/views/MonitorView.vue
@@ -1,7 +1,17 @@
 <template>
-    <p>Monitor</p>
+    <MonitorTopView/>
+    <MonitorLeftView/>
+    <MonitorRightView/>
 </template>
 
-<script>
+<script setup>
+import MonitorLeftView from '../components/monitor/MonitorLeftView.vue';
+import MonitorRightView from '../components/monitor/MonitorRightView.vue';
+import MonitorTopView from '../components/monitor/MonitorTopView.vue';
+
 
 </script>
+
+<style>
+
+</style>
