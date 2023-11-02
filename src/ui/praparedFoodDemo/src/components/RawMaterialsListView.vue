2f725961ad0db2afe1d27526df6c57547349bd14
Yillian4372 <wzhe0769@gmail.com>
2023-11-02 17:48:23 +0800

    原料页面v1.4

--- src/ui/praparedFoodDemo/src/components/RawMaterialsListView.vue
+++ src/ui/praparedFoodDemo/src/components/RawMaterialsListView.vue
@@ -1,25 +1,29 @@
 <template>
+  <div style="padding: 50px;">
     <div class="select">
-        <el-select ref="rawRef" v-model="name" placeholder="请选择原料" @change="$forceUpdate(),toSelect()" value-key="id">
-            <el-option v-for="raw in rawList" :label="raw.name" :value="raw" key="raw.id"></el-option>
-        </el-select>
-    </div>
-    <div class="list">
-        <div class="raw_material">
-          <el-table :data="tableData" style="width: 100%">
-            <!-- 呈现原料的列表 -->
-            <el-table-column prop="name" label="原料名称" width="180" />
-            <el-table-column prop="vendor" label="供应商" width="180" />
-            <el-table-column prop="buy_time" label="购入时间" width="180"/>
-            <el-table-column prop="origin" label="产地" width="180"/>
-            <el-table-column prop="storage" label="储存方式" width="180"/>
-          </el-table>
-        </div>
+      <el-select ref="rawRef" v-model="name" placeholder="请选择原料" @change="$forceUpdate(),toSelect()" value-key="id">
+          <el-option v-for="raw in rawList" :label="raw.name" :value="raw" key="raw.id"></el-option>
+      </el-select>
+      <el-button>查询</el-button>
+  </div>
+  <div class="list">
+      <div class="raw_material">
+        <el-table :data="tableData" style="width: 100%">
+          <!-- 呈现原料的列表 -->
+          <el-table-column prop="name" label="原料名称" width="180" />
+          <el-table-column prop="vendor" label="供应商" width="180" />
+          <el-table-column prop="buy_time" label="购入时间" width="180"/>
+          <el-table-column prop="origin" label="产地" width="180"/>
+          <el-table-column prop="storage" label="储存方式" width="180"/>
+        </el-table>
       </div>
+    </div>
+  </div>
+    
 </template>
 
 <script setup>
-import { onMounted, ref} from 'vue'
+import { onMounted, ref, toHandlerKey} from 'vue'
 import { getRawMaterial } from '@/api/product.js'
 
 let tableData = ref([])
